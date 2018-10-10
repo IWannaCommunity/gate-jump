96b598346427cc9384d66cdb9db0000a9b7c5daa
Klazen <cmurphy1337@live.com>
2018-10-09 21:19:15 -0500

    implement mongo support, graceful server shutdown, user handler, explain environment setup in readme

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
 # gate-jump
-Central Authentication Service for Delfruit, IWM, and other fangame community services
+Central Authentication Service for Delfruit, IWM, and other fangame community services!
+
+## Environment Setup
+
+* Install the MongoDB server: https://www.mongodb.com/download-center?jmp=nav#community
+* Install MongoDB Driver for Go: `go get github.com/mongodb/mongo-go-driver/mongo`
+* Install Gorilla-Mux (golang http server framework) `go get github.com/gorilla/mux`
+* Get MongoDB Compass Community Edition (GUI for MongoDB): https://www.mongodb.com/download-center#compass
+* Open Compass and create a database called `gatejump` and a collection called `users`, and add a user with userid 1:
+
+```
+userid:1 Int64
+username:"Klazen108" String
+password:<a hashed bcrypt password> String
+email:"cmurphy1337@live.com" String
+country:"us" String
+dateCreated:2015-01-26 01:07:08.000 Date
+verified:true Boolean
+banned:false Boolean
+lastToken:"" String
+lastLogin:2018-09-03 07:55:32.000 Date
+lastIP:"127.0.0.1" String
+```
+For more details on the user object format, see the definition of `UserObject` in main.go
 
 ## How to run
 
 To build and run the project:
 
-```go
+```shell
 go build && gate-jump.exe
 ```
 
 Navigate to `http://localhost:10420/`
 
 If you see `{"alive": true}`, you're all set!
+
+Try `http://localhost:10420/user/1` to see your user!
--- main.go
+++ main.go
@@ -1,24 +1,123 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"flag"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
-func main() {
-	log.Printf("Welcome to gate-jump server! Ctrl+C to quit.")
+//shared client,thread-safe: can be used by all request handlers
+var client *mongo.Client
 
+func main() {
+	log.Printf("Welcome to gate-jump server! Setting up environment...")
+
+	//setup database
+	var err error
+	client, err = mongo.NewClient("mongodb://localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO()) //TODO: https://golang.org/pkg/context/
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Database initialized!")
+
+	//setup http server
 	router := mux.NewRouter()
 	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/user/{id}", UserHandler)
 	handler := handlers.RecoveryHandler()(router)
 
-	log.Fatal(http.ListenAndServe(":10420", handler))
+	//from https://github.com/gorilla/mux, "Graceful Shutdown"
+	srv := &http.Server{
+		Addr:         "0.0.0.0:10420",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      handler,
+	}
+	go func() {
+		log.Printf("Starting Server! Ctrl+C to quit.")
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down...")
 }
 
+//HomeHandler handles requests to the root
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, `{"alive": true}`)
+}
+
+//UserHandler handles requests to /user/{id}
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	//get userid from path as integer
+	vars := mux.Vars(r)
+	useridStr := vars["id"]
+	userid, err := strconv.Atoi(useridStr)
+	if err != nil { //if userid can't be converted to integer, tell the client it's WRONG
+		http.Error(w, "Invalid user id", 400)
+		return
+	}
+
+	//get user from database
+	collection := client.Database("gatejump").Collection("users")
+	result := UserObject{}
+	err = collection.FindOne(context.Background(), map[string]int{"userid": userid}).Decode(&result)
+	if err == mongo.ErrNoDocuments { //if no user found, return 404
+		http.Error(w, "No user", 404)
+		return
+	} else if err != nil { //otherwise, panic with the error (500)
+		panic(err)
+	}
+
+	jsonResult, err := json.Marshal(result)
+	if err != nil { //if can't marshal as json, panic with the error (500)
+		panic("Unable to encode json")
+	}
+
+	//all good, tell the client json is incoming, and send it the json
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(jsonResult)
+}
+
+//UserObject is a representation of the user object in the database
+type UserObject struct {
+	Userid      int
+	Username    string
+	Password    string `json:"-"` //don't share the password via json responses
+	Email       string
+	Country     string
+	DateCreated time.Time `bson:"dateCreated"`
+	Verified    bool
+	Banned      bool
+	LastToken   string    `bson:"lastToken"`
+	LastLogin   time.Time `bson:"lastLogin"`
+	LastIP      string    `bson:"lastIP" json:"-"` //don't share the IP via json responses
 }
