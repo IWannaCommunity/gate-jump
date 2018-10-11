86619df352e19cc42cdb5ae7f7584fcb0cdd3762
Evan Rettman <evanmrettman@gmail.com>
2018-10-10 20:42:57 -0400

    added errorLogger.go

--- errorLogger.go
+++ errorLogger.go
@@ -3,7 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"reflect"
+	"runtime"
+	"runtime/debug"
+	"time"
 )
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
 
 func httpError(w http.ResponseWriter, reason string, errorCode int) {
 	if errorCode == 500 { // big boo boo
@@ -20,4 +29,47 @@ func httpError(w http.ResponseWriter, reason string, errorCode int) {
 // just the same thing as http.Error
 func httpResponse(w http.ResponseWriter, reason string, errorCode int) {
 	http.Error(w, reason, errorCode)
+}
+
+// Recover from panics
+func Recover(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			r := recover()
+			if r != nil {
+				log.Printf("Recovered from panic: %v \n %s", r, debug.Stack())
+				http.Error(w, http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}()
+		h.ServeHTTP(w, r)
+	})
+}
+
+// NewLoggingResponseWriter helper function for passing data
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+// Logger logs the information regarding routes in a simple way
+func Logger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		lw := NewLoggingResponseWriter(w)
+		inner.ServeHTTP(lw, r)
+
+		log.Printf(
+			"%s\t%s\t%s\t%d",
+			r.Method,
+			r.RequestURI,
+			time.Since(start),
+			lw.statusCode,
+		)
+	})
+}
+
+// GetFunctionName helper function for returning the function's name
+func GetFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
