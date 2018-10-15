eaeea8f6f19a563e20766f67b191846628f5401e
Evan Rettman <erettman@masonlive.gmu.edu>
2018-10-14 20:18:57 -0400

    minor

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "log"
 
 func main() {
-	log.Printf("Welcome to gate-jump server! Setting up environment...")
+	log.Println("Welcome to gate-jump server! Setting up environment...")
 
 	log.Println("Loading Configuration")
 	LoadConfig("config/config.json")
@@ -11,11 +11,10 @@ func main() {
 	log.Println("Initializing Logging")
 	initLog(Config.Major, Config.Patch, Config.Minor) // major,patch,minor
 
-	log.Printf("Initializing Database")
+	log.Println("Initializing Database")
 	s := Server{}
 	s.Initialize(Config.Database.Username, Config.Database.Password, Config.Database.Dsn)
 
-	log.Printf("Starting the gate-jump server now! Ctrl+C to quit.")
+	log.Println("Starting the gate-jump server now! Ctrl+C to quit.")
 	s.Run(Config.Port)
-
 }
