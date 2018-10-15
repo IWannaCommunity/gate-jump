a5dd4521830d38cc20507974f9abeb13cf48d5a8
Evan Rettman <erettman@masonlive.gmu.edu>
2018-10-14 20:09:33 -0400

    text

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ func main() {
 	log.Println("Initializing Logging")
 	initLog(Config.Major, Config.Patch, Config.Minor) // major,patch,minor
 
-	log.Printf("Preparing Database")
+	log.Printf("Initializing Database")
 	s := Server{}
 	s.Initialize(Config.Database.Username, Config.Database.Password, Config.Database.Dsn)
+
+	log.Printf("Starting the gate-jump server now! Ctrl+C to quit.")
 	s.Run(Config.Port)
 
 }
