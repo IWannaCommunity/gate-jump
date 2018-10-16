2b8ce36eae7e7b4df949647e12e6d4b796ddc4f6
Evan Rettman <erettman@masonlive.gmu.edu>
2018-10-16 15:29:44 -0400

    middleware setup

--- server.go
+++ server.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 )
 
@@ -16,6 +19,19 @@ type Server struct {
 	Router *mux.Router
 	DB     *sql.DB
 }
+
+type Claims struct {
+	Username string `json:"username"`
+	Admin    bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
+const (
+	PUBLIC    = 0
+	USER      = 1
+	ADMIN     = 2
+	ADMINUSER = 3
+)
 
 func (s *Server) Initialize(user, password, dbname string) {
 	var err error
@@ -36,6 +52,10 @@ func (s *Server) InitializeRoutes() {
 	s.Router.HandleFunc("/user/{id}", s.getUser).Methods("GET")
 	s.Router.HandleFunc("/user/{id}", s.updateUser).Methods("PUT")
 	s.Router.HandleFunc("/user/{id}", s.deleteUser).Methods("DELETE")
+	s.Router.Use(s.Recovery)
+	//s.Router.Use(s.Logging)
+	//s.Router.Use(s.JWTContext)
+
 }
 
 func (s *Server) Run(addr string) {
@@ -53,4 +73,63 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
+}
+
+func (s *Server) Recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Recovery Layer Executing")
+		defer func() {
+			if r := recover(); r != nil {
+				log.Println("Recovered from Panic: ", r)
+				respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("%v", r))
+				return
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (s *Server) Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Logging Layer Executing")
+		ts := time.Now()
+		next.ServeHTTP(w, r)
+		// ex: GET /users -> 500 (30.00s)
+		log.Printf("%s\t%s\t%s\t" /*%d*/, r.Method, r.RequestURI, time.Since(ts) /*,lw.statusCode*/)
+	})
+}
+
+/*
+id==JWTid	admin	context	type
+0			0		0		public
+0			1		1		user
+1			0		2		admin
+1			1		3		adminuser
+*/
+
+func (s *Server) JWTContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("JWTContext Layer Executing")
+		claims := Claims{}
+		tokenString := r.Header.Get("Authorization")
+
+		if tokenString == "" { // no token provided. public credential only
+			ctx := context.WithValue(r.Context(), "AUTH", PUBLIC)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}
+		// parse token provided
+		_, err := jwt.ParseWithClaims(tokenString, &claims,
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(Config.JwtSecret), nil
+			})
+		if err != nil { // token couldn't be read. deny completely
+			respondWithError(w, http.StatusUnauthorized, "Invalid token provided")
+			return
+		}
+
+		//if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+
+		//}
+		next.ServeHTTP(w, r)
+	})
 }
