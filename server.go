6126503a1e82892ff769340345cdbd4f0c2605c3
Evan Rettman <erettman@masonlive.gmu.edu>
2018-10-19 23:47:36 -0400

    rearranged ADMINUSER < ADMIN

--- server.go
+++ server.go
@@ -21,8 +21,8 @@ const (
 	CLAIMS    AuthLevel = -1 // just the claims context tag
 	PUBLIC    AuthLevel = 0  // public return
 	USER      AuthLevel = 1  // user return
-	ADMIN     AuthLevel = 2  // admin return
-	ADMINUSER AuthLevel = 3  // admin wants to change password. admins cant change other users passwords so this exists
+	ADMINUSER AuthLevel = 2  // admin wants to change password. admins cant change other users passwords so this exists
+	ADMIN     AuthLevel = 3  // admin return
 	SERVER    AuthLevel = 4  // server can update any user without giving 2 shits
 )
 
