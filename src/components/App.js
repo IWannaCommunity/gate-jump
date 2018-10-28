9093265dc6925a62f72d43e88d0af204c06da94c
MartinWeczerek <martinweczerek@hotmail.com>
2018-10-28 17:28:18 +0100

    Setup an Web-client to start web development

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Test from './test.jsx';
+
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Test/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/test.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>Gate jumps are OP</div>
+        )
+    }
+}
+
+export default Test;
