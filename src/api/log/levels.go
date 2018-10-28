7bbbc77a04818dc4e61b4b6283f2ca7887e45440
Starz0r <starz0r@starz0r.com>
2018-10-28 05:49:12 -0500

    log: move logToFile.go into it's own subpackage module and convert reference to the stdlib logger to our own

---
+++ src/api/log/levels.go
@@ -0,0 +1,34 @@
+package log
+
+import (
+    "log"
+)
+
+func Verbose(argv ...interface{}) {
+    log.Println("[VERBOSE]", argv)
+}
+
+func Debug(argv ...interface{}) {
+    log.Println("[DEBUG]", argv)
+}
+
+func Info(argv ...interface{}) {
+    log.Println("[INFO]", argv)
+}
+
+func Warning(argv ...interface{}) {
+    log.Println("[WARNING]", argv)
+}
+
+func Error(argv ...interface{}) {
+    log.Println("[ERROR]", argv)
+}
+
+func Fatal(argv ...interface{}) {
+    log.Println("[FATAL]", argv)
+    panic(argv)
+}
+
+func Wtf(argv ...interface{}) {
+    log.Println("[WTF]", argv)
+}
