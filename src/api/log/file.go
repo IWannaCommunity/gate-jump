371b9897d5011e0ce461fba57e2bdfdcf388c375
Evan Rettman <evanmrettman@gamil.om>
2018-11-08 15:11:24 -0500

    gofmt

--- src/api/log/file.go
+++ src/api/log/file.go
@@ -1,26 +1,26 @@
 package log
 
 import (
-    "log"
-    "time"
-    "os"
-    "io"
+	"io"
+	"log"
+	"os"
+	"time"
 )
 
 func Init() *os.File {
-    // Make logs directory
-    if _, err := os.Stat("logs"); os.IsNotExist(err) {
-        os.Mkdir("logs", os.ModePerm)
-    }
+	// Make logs directory
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		os.Mkdir("logs", os.ModePerm)
+	}
 
-    // Open a write-only file
-    f, err := os.OpenFile("logs/"+time.Now().Format("2006-01-02")+".log", os.O_WRONLY | os.O_CREATE | os.O_APPEND, 0600)
+	// Open a write-only file
+	f, err := os.OpenFile("logs/"+time.Now().Format("2006-01-02")+".log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
 
-    if err != nil {
-        log.Fatalf("unable to open file for writing: %v", err)
-    }
+	if err != nil {
+		log.Fatalf("unable to open file for writing: %v", err)
+	}
 
-    log.SetOutput(io.MultiWriter(os.Stdout, f))
+	log.SetOutput(io.MultiWriter(os.Stdout, f))
 
-    return f
+	return f
 }
