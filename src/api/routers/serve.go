459ee87d9293f52467858ace1f72c9366d31c4a0
Starz0r <starz0r@starz0r.com>
2018-11-09 00:34:06 -0600

    routers: included httprecovery function from main

--- src/api/routers/serve.go
+++ src/api/routers/serve.go
@@ -1,18 +1,32 @@
 package routers
 
 import (
+    "fmt"
     "net/http"
 
     "github.com/gorilla/mux"
     "github.com/gorilla/handlers"
     "github.com/IWannaCommunity/gate-jump/src/api/log"
     "github.com/IWannaCommunity/gate-jump/src/api/settings"
+    "github.com/IWannaCommunity/gate-jump/src/api/res"
 )
 
 var router *mux.Router
 
 func Serve(port, sslport string) {
     router = mux.NewRouter()
+
+    router.HandleFunc("/", getAlive).Methods("GET")
+    router.HandleFunc("/user", getUsers).Methods("GET")
+    router.HandleFunc("/register", createUser).Methods("POST")
+    router.HandleFunc("/login", validateUser).Methods("POST")
+    router.HandleFunc("/refresh", refreshUser).Methods("POST")
+    router.HandleFunc("/user/{id:[0-9]+}", getUser).Methods("GET")
+    router.HandleFunc("/user/{id}", getUserByName).Methods("GET")
+    router.HandleFunc("/user/{id:[0-9]+}", updateUser).Methods("PUT")
+    router.HandleFunc("/user/{id:[0-9]+}", deleteUser).Methods("DELETE")
+    router.Use(HTTPRecovery)
+    router.Use(JWTContext)
 
     if settings.Https.CertFile != "" && settings.Https.KeyFile != "" {
 		log.Info("HTTPS Credentials picked up, running HTTPS")
@@ -24,4 +38,16 @@ func Serve(port, sslport string) {
 		log.Info("HTTPS Credentials missing, running HTTP")
 		log.Fatal(http.ListenAndServe(":"+port, handlers.LoggingHandler(log.File, router)))
 	}
+}
+
+func HTTPRecovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				res.New(http.StatusInternalServerError).SetErrorMessage(fmt.Sprintf("%v", r)).Error(w)
+				return
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
 }
