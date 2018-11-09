1675eded5c0b32356306850765df12bb210e3dc4
Starz0r <starz0r@starz0r.com>
2018-11-08 22:58:42 -0600

    config: improve settings by making the entire package a key/value store

--- src/api/settings/config.go
+++ src/api/settings/config.go
@@ -7,9 +7,6 @@ import (
 	"github.com/IWannaCommunity/gate-jump/src/api/log"
 )
 
-// Config configuration global variable
-var Config *Configuration
-
 // DatabaseConfig database configuration information (maybe not needed)
 type databaseConfig struct {
 	Username string `json:"username"`
@@ -23,23 +20,23 @@ type httpsConfig struct {
 	KeyFile  string `json:"keyFile"`
 }
 
-// Configuration data structure containing relevant database information (basically hides jwt from github)
-type Configuration struct {
-	Database          databaseConfig `json:"database"`
-	Https             httpsConfig    `json:"https"`
-	RouteBase         string         `json:"routebase"`
-	Port              string         `json:"port"`
-	SslPort           string         `json:"sslPort"`
-	Host              string         `json:"host"`
-	Protocol          string         `json:"protocol"`
-	JwtSecret         string         `json:"jwt_secret"`
-	DiscordWebhookURL string         `json:"discord_webhook_url"`
-	Major             int            `json:"major"`
-	Patch             int            `json:"patch"`
-	Minor             int            `json:"minor"`
-}
+// Configuration and Settings
+var (
+	Database          databaseConfig
+	Https             httpsConfig
+	RouteBase         string
+	Port              string
+	SslPort           string
+	Host              string
+	Protocol          string
+	JwtSecret         string
+	DiscordWebhookURL string
+	Major             int
+	Patch             int
+	Minor             int
+)
 
-// FromFile loads the config.json file into the configuration struct and set Config to it
+// FromFile loads the config.json file into Settings variables
 func FromFile(filename string) error {
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		log.Fatal("Configuration file " + filename + " does not exist!")
@@ -47,13 +44,31 @@ func FromFile(filename string) error {
 
 	file, _ := os.Open(filename)
 	decoder := json.NewDecoder(file)
-	con := new(Configuration)
-	err := decoder.Decode(&con)
+	config := make(map[string]interface{})
+	err := decoder.Decode(&config)
 
 	if err != nil {
 		return err
 	}
 
-	Config = con
+	setActiveConfig(config)
+
 	return nil
+}
+
+func setActiveConfig(configmap map[string]interface{}) {
+	Database = databaseConfig{
+		Username: configmap["database"].(map[string]interface{})["username"].(string),
+		Password: configmap["database"].(map[string]interface{})["password"].(string),
+		Dsn: configmap["database"].(map[string]interface{})["dsn"].(string),
+	}
+
+	Https = httpsConfig{
+		CertFile: configmap["https"].(map[string]interface{})["certFile"].(string),
+		KeyFile: configmap["https"].(map[string]interface{})["keyFile"].(string),
+	}
+
+	Host = configmap["host"].(string)
+	Port = configmap["port"].(string)
+	SslPort = configmap["sslPort"].(string)
 }
