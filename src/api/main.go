24bf8c187de4c283ddc9281d0dc760a00b957fdf
Evan Rettman <evanmrettman@gamil.om>
2018-11-11 17:22:03 -0500

    main: remove router import & gofmt

--- src/api/main.go
+++ src/api/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"github.com/IWannaCommunity/gate-jump/src/api/database"
 	"github.com/IWannaCommunity/gate-jump/src/api/log"
 	"github.com/IWannaCommunity/gate-jump/src/api/settings"
-	"github.com/IWannaCommunity/gate-jump/src/api/database"
-	"github.com/IWannaCommunity/gate-jump/src/api/routers"
 )
 
 func main() {
