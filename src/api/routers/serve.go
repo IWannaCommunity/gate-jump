ba6cacf5138adcacee9ebe1c3eb3a6da562ca7db
Evan Rettman <evanmrettman@gamil.om>
2018-11-11 15:09:08 -0500

    added getrouter function to try to access router for test suite
    failure

--- src/api/routers/serve.go
+++ src/api/routers/serve.go
@@ -1,40 +1,40 @@
 package routers
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/gorilla/mux"
-    "github.com/gorilla/handlers"
-    "github.com/IWannaCommunity/gate-jump/src/api/log"
-    "github.com/IWannaCommunity/gate-jump/src/api/settings"
-    "github.com/IWannaCommunity/gate-jump/src/api/res"
-    "github.com/IWannaCommunity/gate-jump/src/api/authentication"
+	"github.com/IWannaCommunity/gate-jump/src/api/authentication"
+	"github.com/IWannaCommunity/gate-jump/src/api/log"
+	"github.com/IWannaCommunity/gate-jump/src/api/res"
+	"github.com/IWannaCommunity/gate-jump/src/api/settings"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 var router *mux.Router
 
 func Serve(port, sslport string) {
-    router = mux.NewRouter()
+	router = mux.NewRouter()
 
-    router.HandleFunc("/", getAlive).Methods("GET")
-    router.HandleFunc("/user", getUsers).Methods("GET")
-    router.HandleFunc("/register", createUser).Methods("POST")
-    router.HandleFunc("/login", validateUser).Methods("POST")
-    router.HandleFunc("/refresh", refreshUser).Methods("POST")
-    router.HandleFunc("/user/{id:[0-9]+}", getUser).Methods("GET")
-    router.HandleFunc("/user/{id}", getUserByName).Methods("GET")
-    router.HandleFunc("/user/{id:[0-9]+}", updateUser).Methods("PUT")
-    router.HandleFunc("/user/{id:[0-9]+}", deleteUser).Methods("DELETE")
-    router.Use(HTTPRecovery)
-    router.Use(authentication.JWTContext)
+	router.HandleFunc("/", getAlive).Methods("GET")
+	router.HandleFunc("/user", getUsers).Methods("GET")
+	router.HandleFunc("/register", createUser).Methods("POST")
+	router.HandleFunc("/login", validateUser).Methods("POST")
+	router.HandleFunc("/refresh", refreshUser).Methods("POST")
+	router.HandleFunc("/user/{id:[0-9]+}", getUser).Methods("GET")
+	router.HandleFunc("/user/{id}", getUserByName).Methods("GET")
+	router.HandleFunc("/user/{id:[0-9]+}", updateUser).Methods("PUT")
+	router.HandleFunc("/user/{id:[0-9]+}", deleteUser).Methods("DELETE")
+	router.Use(HTTPRecovery)
+	router.Use(authentication.JWTContext)
 
-    if settings.Https.CertFile != "" && settings.Https.KeyFile != "" {
+	if settings.Https.CertFile != "" && settings.Https.KeyFile != "" {
 		log.Info("HTTPS Credentials picked up, running HTTPS")
 		log.Fatal(http.ListenAndServeTLS(":"+sslport,
-            settings.Https.CertFile,
-            settings.Https.KeyFile,
-            handlers.LoggingHandler(log.File, router)))
+			settings.Https.CertFile,
+			settings.Https.KeyFile,
+			handlers.LoggingHandler(log.File, router)))
 	} else {
 		log.Info("HTTPS Credentials missing, running HTTP")
 		log.Fatal(http.ListenAndServe(":"+port, handlers.LoggingHandler(log.File, router)))
@@ -51,4 +51,8 @@ func HTTPRecovery(next http.Handler) http.Handler {
 		}()
 		next.ServeHTTP(w, r)
 	})
+}
+
+func GetRouter() *mux.Router {
+	return router
 }
