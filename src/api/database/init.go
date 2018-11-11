66bac573d12619a013acee33b733ce36c36e54fc
Evan Rettman <evanmrettman@gamil.om>
2018-11-11 17:20:52 -0500

    database: InitServer used to start server

--- src/api/database/init.go
+++ src/api/database/init.go
@@ -1,21 +1,33 @@
 package database
 
 import (
-    "fmt"
-    "database/sql"
+	"database/sql"
+	"fmt"
+	"gate-jump/src/api/routers"
 
-    _ "github.com/go-sql-driver/mysql"
-    "github.com/IWannaCommunity/gate-jump/src/api/log"
+	"github.com/IWannaCommunity/gate-jump/src/api/log"
+	"github.com/IWannaCommunity/gate-jump/src/api/settings"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 var db *sql.DB
 
-func Connect(user, password, dbname string) {
+func InitServer() (*sql.DB, *mux.Router) {
+	db := connect(settings.Database.Username,
+		settings.Database.Password,
+		settings.Database.Dsn)
+	router := routers.Serve(settings.Port, settings.SslPort)
+	return db, router
+}
+
+func connect(user, password, dbname string) *sql.DB {
 	var err error
 	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@/%s?charset=utf8mb4&parseTime=True&interpolateParams=true", user, password, dbname))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-    log.Debug(SetupUsers())
+	log.Debug(SetupUsers())
+	return db
 }
