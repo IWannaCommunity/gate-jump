a92b6d3ca6aeb55e1ce11065d11d39fa0f5dc76f
Starz0r <starz0r@starz0r.com>
2018-11-18 03:15:06 -0600

    database: go fmt

--- src/api/database/setup.go
+++ src/api/database/setup.go
@@ -1,54 +1,55 @@
 package database
 
 import (
-    "strings"
-    "database/sql"
+	"database/sql"
+	"strings"
 
-    "github.com/IWannaCommunity/gate-jump/src/api/settings"
-    "github.com/IWannaCommunity/gate-jump/src/api/migrations"
+	"github.com/IWannaCommunity/gate-jump/src/api/log"
+	"github.com/IWannaCommunity/gate-jump/src/api/migrations"
+	"github.com/IWannaCommunity/gate-jump/src/api/settings"
 )
 
 func doesTableExist(name string) (error, bool) {
-    stmt, err := db.Prepare(`SELECT *
+	stmt, err := db.Prepare(`SELECT *
         FROM information_schema.tables
         WHERE table_schema = ?
             AND table_name = ?
         LIMIT 1;`)
 
-    if err != nil {
-        return err, true
-    }
+	if err != nil {
+		return err, true
+	}
 
-    rows := stmt.QueryRow(settings.Database.Dsn, name)
+	rows := stmt.QueryRow(settings.Database.Dsn, name)
 
-    table := make(map[string]interface{})
-    err = rows.Scan(&table)
+	table := make(map[string]interface{})
+	err = rows.Scan(&table)
 
-    if err == sql.ErrNoRows {
-        return nil, false
-    }
+	if err == sql.ErrNoRows {
+		return nil, false
+	}
 
-    //TODO: this should be it's own error through errors.New but I'm lazy
-    return err, true
+	//TODO: this should be it's own error through errors.New but I'm lazy
+	return err, true
 }
 
 func setupSchema(filename string) error {
-    f, err := migrations.ReadFile("src/migrations/"+filename)
-    buf := new(strings.Builder)
-    buf.Write(f)
+	f, err := migrations.ReadFile("src/migrations/" + filename)
+	buf := new(strings.Builder)
+	buf.Write(f)
 
-    stmt, err := db.Prepare(buf.String())
+	stmt, err := db.Prepare(buf.String())
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    _, err = stmt.Exec()
+	_, err = stmt.Exec()
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    //TODO: Probably should return the result, and the error if it's not nil
-    return nil
+	//TODO: Probably should return the result, and the error if it's not nil
+	return nil
 }
