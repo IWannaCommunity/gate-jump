b17119bd572bb8637da6d363434379fb2ae21e91
Starz0r <starz0r@starz0r.com>
2018-11-18 03:36:57 -0600

    log: removed spacing from log formatting

--- src/api/log/levels.go
+++ src/api/log/levels.go
@@ -6,36 +6,36 @@ import (
 
 //Verbose logs additional info, or anything that isn't important
 func Verbose(argv ...interface{}) {
-	log.Println("[VERBOSE ]\t", argv)
+	log.Printf("[VERBOSE]\t", argv)
 }
 
 //Debug logs information that could be helpful when debugging
 func Debug(argv ...interface{}) {
-	log.Println("[ DEBUG ]\t", argv)
+	log.Printf("[DEBUG]\t", argv)
 }
 
 //Info logs information that could be important
 func Info(argv ...interface{}) {
-	log.Println("[  INFO ]\t", argv)
+	log.Printf("[INFO]\t", argv)
 }
 
 //Warning logs information that points to a possible issue or point of failure
 func Warning(argv ...interface{}) {
-	log.Println("[WARNING]\t", argv)
+	log.Printf("[WARNING]\t", argv)
 }
 
 //Error logs information about an issue that actually occured
 func Error(argv ...interface{}) {
-	log.Println("[ ERROR ]\t", argv)
+	log.Printf("[ERROR]\t", argv)
 }
 
 //Fatal any last words?
 func Fatal(argv ...interface{}) {
-	log.Println("[ FATAL ]\t", argv)
+	log.Printf("[FATAL]\t", argv)
 	panic(argv)
 }
 
 //Wtf this shouldn't happen
 func Wtf(argv ...interface{}) {
-	log.Println("[  WTF  ]\t", argv)
+	log.Printf("[WTF]\t", argv)
 }
