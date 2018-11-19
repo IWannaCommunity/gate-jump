3047ab336361b7f856fc44a5d537e8f1a1635512
Evan Rettman <evanmrettman@gamil.om>
2018-11-18 21:03:38 -0500

    database user: look based on name not id for getUserByName

--- src/api/database/users.go
+++ src/api/database/users.go
@@ -87,9 +87,9 @@ func (u *User) GetUser(auth authentication.Level) res.ServerError {
 func (u *User) GetUserByName(auth authentication.Level) res.ServerError {
 	var serr res.ServerError
 	if auth > authentication.USER { // deleted search check
-		serr.Query = "SELECT * FROM users WHERE id=?"
+		serr.Query = "SELECT * FROM users WHERE name=?"
 	} else {
-		serr.Query = "SELECT * FROM users WHERE id=? AND deleted=FALSE"
+		serr.Query = "SELECT * FROM users WHERE name=? AND deleted=FALSE"
 	}
 	serr.Args = append(serr.Args, u.Name)
 	serr.Err = u.ScanAll(db.QueryRow(serr.Query, serr.Args...))
