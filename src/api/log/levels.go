59ac6a2d5b5c40cf7ffbb93c0c0bf2b480cdc821
Starz0r <starz0r@starz0r.com>
2018-12-13 19:50:35 -0600

    log: making logs readable again by using println instead of printf

--- src/api/log/levels.go
+++ src/api/log/levels.go
@@ -1,41 +1,42 @@
 package log
 
 import (
+	//"fmt"
 	"log"
 )
 
 //Verbose logs additional info, or anything that isn't important
 func Verbose(argv ...interface{}) {
-	log.Printf("[VERBOSE]\t", argv)
+	log.Println("[VERBOSE]\t", argv)
 }
 
 //Debug logs information that could be helpful when debugging
 func Debug(argv ...interface{}) {
-	log.Printf("[DEBUG]\t", argv)
+	log.Println("[DEBUG]\t", argv)
 }
 
 //Info logs information that could be important
 func Info(argv ...interface{}) {
-	log.Printf("[INFO]\t", argv)
+	log.Println("[INFO]\t", argv)
 }
 
 //Warning logs information that points to a possible issue or point of failure
 func Warning(argv ...interface{}) {
-	log.Printf("[WARNING]\t", argv)
+	log.Println("[WARNING]\t", argv)
 }
 
 //Error logs information about an issue that actually occured
 func Error(argv ...interface{}) {
-	log.Printf("[ERROR]\t", argv)
+	log.Println("[ERROR]\t", argv)
 }
 
 //Fatal any last words?
 func Fatal(argv ...interface{}) {
-	log.Printf("[FATAL]\t", argv)
+	log.Println("[FATAL]\t", argv)
 	panic(argv)
 }
 
 //Wtf this shouldn't happen
 func Wtf(argv ...interface{}) {
-	log.Printf("[WTF]\t", argv)
+	log.Println("[WTF]\t", argv)
 }
