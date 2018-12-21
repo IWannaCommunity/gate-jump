debdaf8eaa7703785c48d81f6c3c79e2f0f34afe
Starz0r <starz0r@starz0r.com>
2018-12-20 22:46:38 -0600

    routers: create a magiclink when a user registers an account

--- src/api/routers/users.go
+++ src/api/routers/users.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/IWannaCommunity/gate-jump/src/api/authentication"
@@ -118,6 +119,13 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 
 	checkuser := u
 
+	// Generate a random string that will be used for email verification in the background
+	ml := *new(database.MagicLink)
+	cherr := make(chan error)
+	chstr := make(chan []byte)
+	go util.CreateRandomString(32, 1, chstr, cherr)
+
+	// Validate user input
 	if !util.IsValidUsername(*checkuser.Name) || util.IsValidEmail(*checkuser.Name) {
 		res.New(http.StatusBadRequest).SetErrorMessage("Invalid Username Format").Error(w)
 		return
@@ -163,6 +171,24 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 		res.New(http.StatusInternalServerError).SetInternalError(&serr).Error(w)
 		return
 	}
+
+	// create a new magic link
+	err = <-cherr
+	if err != nil {
+		// we can't "fail" here, so we will pass and ask them manually ask for a new magiclink
+		log.Error("Could not generate a random magiclink string, %v", err)
+	}
+
+	// TODO: Probably don't need strings.Builder, maybe do string(<-chstr)
+	buf := new(strings.Builder)
+	buf.Write(<-chstr)
+	ml.UserID = u.ID
+	ml.Magic = buf.String()
+	if serr := ml.CreateMagicLink(); serr.Err != nil {
+		// can't fail here either
+		log.Error("Could not create a magiclink, %v", serr.Err)
+	}
+	log.Info("New Magiclink ID: ", ml.ID)
 
 	res.New(http.StatusCreated).JSON(w)
 }
