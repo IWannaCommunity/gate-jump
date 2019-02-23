dc25a59da8bde686d8f00da251a94593f8f3b344
Starz0r <starz0r@starz0r.com>
2019-02-23 17:35:34 -0600

    schemas: allow for a larger description

--- src/schemas/00005_scopes.sql
+++ src/schemas/00005_scopes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE scopes (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
     PRIMARY KEY (id)
 )
