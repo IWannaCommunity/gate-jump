d7989c847c3989ca43701edb247aa359c68f3e78
Evan Rettman <erettman@masonlive.gmu.edu>
2019-03-16 21:16:35 -0400

    testing: initial create user tests. these fail currently

--- src/api/routers/users_test.go
+++ src/api/routers/users_test.go
@@ -79,3 +79,94 @@ func TestAlive(t *testing.T) {
 	}
 
 }
+
+func TestCreateUser(t *testing.T) {
+	te.Prepare("POST", "/register")
+
+	var badRequests []string // only valid request should be one that contains name, email, and password
+
+	badRequests = append(badRequests,
+		`sdfdrslkjgnm4momgom!!!`,                                                             // jibberish
+		`{"password":"12345678","email":"email@website.com"}`,                                // missing name
+		`{"name":"test_user","email":"email@website.com"}`,                                   // missing password
+		`{"name":"test_user","password":"12345678"}`,                                         // missing email
+		`{"name":"test_user","password":"12345678","country":"us","locale":"en"}`,            // extra
+		`{"name":"12356","password":"12345678","email":"email@website.com"}`,                 // invalid username (all numerics)
+		`{"name":"test_user@website.com","password":"12345678","email":"email@website.com"}`, // invalid username (its an email)
+		`{"name":"test_user","password":"12345","email":"email@website.com"}`,                // invalid password (less than 8 characters)
+		`{"name":"test_user","password":"12345678","email":"email"}`)                         // invalid email (non-email format)
+	mainUser := `{"name":"test_user","password":"12345678","email":"email@website.com"}`               // valid request
+	duplicateName := `{"name":"test_user","password":"12345678","email":"email@someotherwebsite.com"}` // name == mainUser.Name
+	duplicateEmail := `{"name":"some_other_user","password":"12345678","email":"email@website.com"}`   // email == mainUser.Email
+
+	// test bad request
+	for i, badRequest := range badRequests {
+		r := te.Request([]byte(badRequest))
+		if assert.NoError(t, r.Err, te.Expect()) {
+
+			assert.Equal(t, http.StatusBadRequest, r.Code, te.Expect())
+			assert.False(t, r.Response.Success, te.Expect())
+			if assert.NotNil(t, r.Response.Error, te.Expect()) {
+				switch i {
+				case 5:
+					fallthrough
+				case 6: // invalid username
+					assert.Equal(t, "Invalid Username Format", *r.Response.Error, te.Expect())
+				case 7:
+					assert.Equal(t, "Invalid Password Format", *r.Response.Error, te.Expect())
+				case 8:
+					assert.Equal(t, "Invalid Email Format", *r.Response.Error, te.Expect())
+				default:
+					assert.Equal(t, "Invalid Request Payload", *r.Response.Error, te.Expect())
+				}
+			}
+
+			assert.Nil(t, r.Response.Token, te.Expect())
+			assert.Nil(t, r.Response.User, te.Expect())
+			assert.Nil(t, r.Response.UserList, te.Expect())
+		}
+	}
+
+	// test creating a user
+	r := te.Request([]byte(mainUser))
+	if assert.NoError(t, r.Err) {
+
+		assert.Equal(t, http.StatusCreated, r.Code, te.Expect())
+		assert.True(t, r.Response.Success, te.Expect())
+
+		assert.Nil(t, r.Response.Error, te.Expect())
+		assert.Nil(t, r.Response.Token, te.Expect())
+		assert.Nil(t, r.Response.User, te.Expect())
+		assert.Nil(t, r.Response.UserList, te.Expect())
+	}
+	// test duplicate username
+	r = te.Request([]byte(duplicateName))
+	if assert.NoError(t, r.Err) {
+
+		assert.Equal(t, http.StatusConflict, r.Code, te.Expect())
+		assert.False(t, r.Response.Success, te.Expect())
+		if assert.NotNil(t, r.Response.Error) {
+			assert.Equal(t, "Username Already Exists", *r.Response.Error, te.Expect())
+		}
+
+		assert.Nil(t, r.Response.Token)
+		assert.Nil(t, r.Response.User)
+		assert.Nil(t, r.Response.UserList)
+	}
+
+	// test duplicate email
+	r = te.Request([]byte(duplicateEmail))
+	if assert.NoError(t, r.Err) {
+
+		assert.Equal(t, http.StatusConflict, r.Code, "expected statusconflict")
+		assert.False(t, r.Response.Success)
+		if assert.NotNil(t, r.Response.Error) {
+			assert.Equal(t, "Email Already In Use", *r.Response.Error)
+		}
+
+		assert.Nil(t, r.Response.Token)
+		assert.Nil(t, r.Response.User)
+		assert.Nil(t, r.Response.UserList)
+	}
+
+}
