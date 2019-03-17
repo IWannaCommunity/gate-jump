bc6bbd86aaf51970f402a9ef97ee4d7c2988094e
Evan Rettman <erettman@masonlive.gmu.edu>
2019-03-17 01:00:17 -0400

    testing: adding initalized function for testing package in database init.go

--- src/api/database/init.go
+++ src/api/database/init.go
@@ -12,6 +12,10 @@ const version uint8 = 5
 
 var db *sql.DB
 
+func Initialized() bool {
+	return db != nil
+}
+
 func Connect(user, password, dbname string) {
 	var err error
 	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@/%s?charset=utf8mb4&parseTime=True&interpolateParams=true", user, password, dbname))
