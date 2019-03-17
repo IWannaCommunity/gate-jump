e228959312fa60c5c0bfbdbaf68c24f7ffe6bb3b
Starz0r <starz0r@starz0r.com>
2019-03-17 03:42:41 -0500

    schemas: default privileged group

---
+++ src/schemas/00014_defaultgroup.sql
@@ -0,0 +1,2 @@
+INSERT INTO groups (name, description)
+VALUES ('passport', 'Default Passport Group, to allow for the creation, modifying, and deletion, of users, groups, and scopes, by trusted people.');
