20fc2dcf2a31336321010911887fe821630fbc2d
Evan Rettman <erettman@masonlive.gmu.edu>
2019-05-23 01:58:28 -0400

    routers: created token/user shell functions

--- src/api/routers/token.go
+++ src/api/routers/token.go
@@ -1,1 +1,19 @@
 package routers
+
+import (
+	"errors"
+
+	mux "github.com/spidernest-go/mux"
+)
+
+func createToken(mux.Context) error {
+	return errors.New("Not Implemented")
+}
+
+func updateToken(mux.Context) error {
+	return errors.New("Not Implemented")
+}
+
+func deleteToken(mux.Context) error {
+	return errors.New("Not Implemented")
+}
--- src/api/routers/users.go
+++ src/api/routers/users.go
@@ -1,1 +1,23 @@
 package routers
+
+import (
+	"errors"
+
+	mux "github.com/spidernest-go/mux"
+)
+
+func createUser(mux.Context) error {
+	return errors.New("Not Implemented")
+}
+
+func updateUser(mux.Context) error {
+	return errors.New("Not Implemented")
+}
+
+func verifyUser(mux.Context) error {
+	return errors.New("Not Implemented")
+}
+
+func deleteUser(mux.Context) error {
+	return errors.New("Not Implemented")
+}
