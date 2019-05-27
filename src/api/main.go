cc32a8d9b1328b31eb50297080f23ba729f17f67
Evan Rettman <erettman@masonlive.gmu.edu>
2019-05-26 23:30:01 -0400

    Moved individual components of starting the server to seperate functions

--- src/api/main.go
+++ src/api/main.go
@@ -19,55 +19,69 @@ var (
 )
 
 func main() {
-	var err error
-	// Check if we are in debugging mode.
+	SetupLogging()      // Check for debug mode and apply settings appropriately,
+	BuildInfo()         // Print build info to log.
+	LoadConfiguration() // Load the configuration data into the server.
+	// StartMailer() // Begin running the mailer service.
+	// ConnectDatabase() // Connect to the database.
+	// VerifySchemas() // Verify that the database schemas are correct.
+	ListenAndServe() // Listen and serve API requests.
+}
+
+// CheckDebug checks if the argument "debug" for the first
+func SetupLogging() {
 	if len(os.Args) > 1 && os.Args[1] == "debug" {
 		log.Stdout()
 		log.Info().Msg("Server started in debugging mode. Logging exclusively to stdout...")
 	} else {
 		// log.Configure(...)
 	}
+}
 
-	// Compile build info.
+func BuildInfo() {
 	log.Info().Msgf("Starting passport: %s", fmt.Sprintf("%s.%s.%s %s, commit: %s, compiled @ %s", Minor, Patch, Major, Extra, Commit, Date))
+}
 
-	// Load configuration data.
+func LoadConfiguration() {
 	log.Info().Msg("Loading configuration data...")
-	err = settings.FromFile("config/config.json")
+	err := settings.FromFile("config/config.json")
 	if err != nil {
 		log.Fatal().Msgf("Failed loading configuration data: %v", err)
 	}
+}
 
-	// Initalize the mailer.
+func StartMailer() {
+	log.Info().Msg("Initalizing mailer...")
 	/*
-		log.Info().Msg("Initalizing mailer...")
-			err = mailer.SMTPInit()
-			if err != nil {
-				log.Fatal().Msgf("Failed initalizing mailer: %v", err)
-			}
-			go mailer.Daemon()
+		err := mailer.SMTPInit()
+		if err != nil {
+			log.Fatal().Msgf("Failed initalizing mailer: %v", err)
+		}
+		go mailer.Daemon()
 	*/
+}
 
-	// Initalize the database.
+func ConnectDatabase() {
+	log.Info().Msg("Initalizing database...")
 	/*
-		log.Info().Msg("Initalizing database...")
 		database.Connect(
 			settings.Database.Username,
 			settings.Database.Password,
 			settings.Database.Dsn)
 	*/
+}
 
-	// Verify database schema.
+func VerifySchemas() {
+	log.Info().Msg("Verifying database schema...")
 	/*
-		log.Info().Msg("Verifying database schema...")
 		err = database.Init()
 		if err != nil {
 			log.Fatal().Msgf("Database could not initalize: %v", err)
 		}
 	*/
+}
 
-	// Preparing to serve. https://github.com/spidernest-go/mux
+func ListenAndServe() {
 	log.Info().Msgf("Router listening at %s:%s", settings.Host, settings.Port)
 	routers.Serve(settings.APIVer, settings.Port, settings.SslPort)
-
 }
